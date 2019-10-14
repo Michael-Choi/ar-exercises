7e2ab52dc7c701901948f08e126c05f1edd7894a
Michael-Choi <Michael.choi@queensu.ca>
2019-10-14 18:54:45 +0000

    create 3 stores using active record

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,3 +4,8 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+burnaby=Store.create(name: "burnaby", mens_apparel: true, womens_apparel: true, annual_revenue: 300000)
+richmond=Store.create(name: "richmond", womens_apparel: true, annual_revenue: 1260000)
+gastown=Store.create(name: "gastown", mens_apparel: true, annual_revenue: 190000)
+
+puts Store.count
